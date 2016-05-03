28c0b47786c02fcb6fc4d0b80d0717c95e0b5efd
Sergio Alba <sergio.alba.ceacero@gmail.com>
2016-05-03 17:18:24 +0200

    ontingencia SSS

--- SH/SSS-Contingencia/SSS-ORA-Contingencia.sh
+++ SH/SSS-Contingencia/SSS-ORA-Contingencia.sh
@@ -30,20 +30,25 @@ ASM_HOME=`pwdx $ASM_PID | awk -F ": " '{print $2}' | awk -F "/" '{print $1 "/"$2
 ASM_USER=`ps -ef | grep asm_pmon | grep -v grep | awk '{print $1}'`
 TEMPDIR=`mktemp -d "/tmp/XXXXXXX"`
 ENVFILE=$TEMPDIR/asm.env
+LSDG=$TEMDIR/lsdg.sql
+
 ######
 #MAIN#
 ######
 chmod -R 777 $TEMPDIR
-cat <<EOF >$TEMPDIR/asm.env
+touch $TEMPDIR/asm.env
+touch $TEMPDIR/lsdg.sql
+touch $TEMPDIR/lsdg.log
+chmod 777 $TEMPDIR/*.*
+
+cat <<EOF >>$ENVFILE
 export ORACLE_SID=$ASM_SID
 export ORACLE_HOME=$ASM_HOME
-
-EOF
-cat <<EOF >$TEMPDIR/lsdg
-lsdg
-
 EOF
 
-runuser -l $ASM_USER -c ". ${ENVFILE} ; $ASM_HOME/bin/asmcmd << $TEMPDIR/lsdg " > $TEMPDIR/lsdg.out
+cat <<EOF >>$LSDG
+select name, state, type, total_mb, free_mb from v$asm_diskgroup;
+exit;
+EOF
 
-cat $TEMPDIR/lsdg.out
+#/sbin/runuser -l $ASM_USER -c ". ${ENVFILE} ; $ASM_HOME/bin/sqlplus -s / as sysdba @$TEMDIR/lsdg.sql" > $TEMFILE/lsdg.log
